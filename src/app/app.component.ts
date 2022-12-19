7b701e77e61c074424b91e87ad809e1b2c5f3760
ramanand28 <arc23453@gmail.com>
2022-12-20 00:47:38 +0530

    angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'hello-world';
+  click='not clicked'
+  onclick(){
+    this.click='button clicked';
+  }
+}
